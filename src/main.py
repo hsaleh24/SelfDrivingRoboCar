8d24c001d6340c04459068c484b36eada81dae53
hsaleh24 <hsalehh24@gmail.com>
2018-08-18 17:19:30 -0400

    Distance sensor working (but needs multithreading)

--- src/main.py
+++ src/main.py
@@ -2,41 +2,88 @@ import RPi.GPIO as GPIO
 import time
 
 
+ENABLE = 13
+
+ECHO = 14
+TRIG = 15
+
+SERVO = 12 #PWM pin
+
 def Main():
-    #GPIOSetup()
+    print "Main"
+    GPIOSetup()
+    #TestMotor()
+    GPIO.cleanup()
     return;
+
+def TestMotor():
+    print "TestMotor"
+    GPIO.output(ENABLE, GPIO.HIGH)
+    GPIO.output(19, GPIO.HIGH)
+    GPIO.output(26, GPIO.LOW)
+    time.sleep(10)
+    GPIO.output(ENABLE, GPIO.LOW)
+    GPIO.output(19, GPIO.LOW)
+    GPIO.output(26, GPIO.LOW)
+    print "Done"
 
 
 def GPIOSetup():
+    print "GPIOSetup"
+    
     GPIO.setmode(GPIO.BCM) #reference pins with BCM vs BOARD numbering
 
     # MOTOR CONTROL
-    GPIO.setup(13, GPIO.OUT) # ENABLE PIN
+    GPIO.setup(ENABLE, GPIO.OUT) # ENABLE PIN
     GPIO.setup(19, GPIO.OUT) # 1 of 2 input pins
     GPIO.setup(26, GPIO.OUT) # 2 of 2 input pins
 
     # SERVO CONTROL
-    GPIO.setup(23, GPIO.OUT) # GRND
-    GPIO.setup(24, GPIO.OUT) # SIGNAL to control angle (PWM)
+    GPIO.setup(SERVO, GPIO.OUT) # SIGNAL to control angle (PWM)
 
     # DISTANCE SENSOR
-    GPIO.setup(14, GPIO.IN)  # ECHO PIN
-    GPIO.setup(15, GPIO.OUT) # TRIGGER PIN
+    GPIO.setup(ECHO, GPIO.IN)  # ECHO PIN
+    GPIO.setup(TRIG, GPIO.OUT) # TRIGGER PIN
+    StartDistanceSensor()
     
     # camera - does it go here? ...is it GPIO?
     return;
 
 
 def StartDistanceSensor():
-    GPIO.output(14, GPIO.HIGH)
+    print "StartDistanceSensor"
+
+    GPIO.output(TRIG, GPIO.LOW)
+    time.sleep(2)
+    
+    GPIO.output(TRIG, GPIO.HIGH)
     time.sleep(0.000010) # 10 microsecond
-    GPIO.output(14, GPIO.LOW)
+    GPIO.output(TRIG, GPIO.LOW)
+    
+    MonitorDistanceSensor() # start new task of MonitorDistanceSensor
     return;
 
 
 def MonitorDistanceSensor(): # setup as a seperate task
-    return;
+    print "MonitorDistanceSensor"
+
+    # get the time of the rising edge of the pulse
+    while GPIO.input(ECHO) == 0:
+        pulse_start = time.time()
+
+    # get the time of the falling edge of the pulse
+    while GPIO.input(ECHO) == 1:
+        pulse_end = time.time()
+
+    # get time of pulse and use to calculate distance
+    pulse_time = pulse_end - pulse_start
+    distance_cm = pulse_time*34300/2; #(convert time to dist give speed of sound = 343m/s)
+
+    print "Distance: ",distance_cm,"cm"
     
+    return;
+
+Main()
+
 ## should setup all sensors as their own task/thread
-## also make sure to put the pin numbers as constants
 ## double check PWM pin, i.e. which pins can be setup as PWM on rasp pi 3 and how to do
